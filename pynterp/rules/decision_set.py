c8dbf136503a2a8d45130b2acd49180e6ca275d0
Dylan Randle <dylanrandle@rogers.com>
2019-11-27 11:17:02 -0500

    some slight improvements in terms of code organization; allow saving cached computation for use in a different DecisionSet

--- pynterp/rules/decision_set.py
+++ pynterp/rules/decision_set.py
@@ -85,19 +85,27 @@ class DecisionSet():
     Implementation of Decision Sets
     See here: https://www-cs-faculty.stanford.edu/people/jure/pubs/interpretable-kdd16.pdf
     """
-    def __init__(self, min_support = 0.01, lambdas = [1, 1, 1, 1, 100, 1, 100]):
+    def __init__(self, min_support = 0.01, lambdas = [186, 732, 160, 950, 800, 752, 281]):
         """
         Initializes a DecisionSet
 
         Input:
             min_support: float in (0, 1) denoting minimum proportion of rows an itemset can cover
             lambdas: len=7 iterable; values to use for weighting the loss function (lambdas in paper)
+                     default [186, 732, 160, 950, 800, 752, 281], found from random search on Titanic Dataset
         """
         self.min_support = min_support
         self.lambdas = lambdas
-        assert len(self.lambdas) == 7, '`lambdas` must be exactly length 7!'
+        assert len(self.lambdas) == 7, 'lambdas must be exactly length 7!'
+        # `d` (aka delta) values are set by the algorithm in paper
+        # optimality of submodular approach is guaranteed for these values
+        # assume that we do not want to change them, so they are hard-coded
+        self.d1 = 1/3
+        self.d2 = 1/3
+        self.d3 = -1
+        self.cache_exists = False
 
-    def fit(self, X, y, d1=1/3, d2=1/3, d3=-1, max_est_iter=10):
+    def fit(self, X, y, max_est_iter=10):
         """
         Fits a DecisionSet to a dataset X (one-hot encoded) with labels y (categorical)
 
@@ -105,15 +113,18 @@ class DecisionSet():
             X: pandas DataFrame containing one-hot encoded (True/False or 1/0) values.
                X should have shape (n, p) where n is number of points and p is number of features
             y: pandas Series containing class labels
-            d*: delta parameters. d1 : delta, d2/d3 : delta' (two settings)
             max_est_iter: maximum iterations for estimating w
         """
-        itemsets = self.mine_itemsets(X)
-        self.L_max = np.max(itemsets.apply(lambda x: len(x)))
+        if not self.cache_exists:
+            self.cache(X, y)
+
+        self.L_max = np.max(self.itemsets.apply(lambda x: len(x)))
+        self.classes = y.unique()
         self.default_pred = y.value_counts().idxmax()
+        self.max_est_iter = max_est_iter
 
         # two runs each with different d' (d2 vs d3)
-        dset1, obj_val1, dset2, obj_val2 = self.smooth_local_search(X, y, itemsets, d1=d1, d2=d2, d3=d3, max_est_iter=max_est_iter)
+        dset1, obj_val1, dset2, obj_val2 = self.smooth_local_search(X, y, max_est_iter=max_est_iter)
 
         # take best of both runs
         if obj_val1 > obj_val2:
@@ -121,20 +132,16 @@ class DecisionSet():
         else:
             self.decision_set = dset2
 
-        # sort by score for tie-breaking (and general presentation)
+        # sort by accuracy score for tie-breaking (and general presentation)
         print('Calculating individual rule scores')
-        # f1_list = []
         acc_list = []
         for i, r in self.decision_set.iterrows():
             xcov = cover(X, r['item'])
             ycov = y.loc[xcov.index]
             pred = r['class'] * np.ones_like(ycov.values)
             if len(xcov) > 0:
-                # f1 = f1_score(ycov, pred, labels=np.unique(pred))
                 acc = accuracy_score(ycov, pred)
-                # f1_list.append(f1)
                 acc_list.append(acc)
-        # self.decision_set['f1'] = f1_list
         self.decision_set['acc'] = acc_list
         self.decision_set = self.decision_set.sort_values(by='acc', ascending=False) # prioritize accuracy
 
@@ -145,7 +152,7 @@ class DecisionSet():
     def predict(self, X):
         """
         make predictions for every element of X (pandas DataFrame)
-        uses highest-F1 rule that applies, otherwise uses default
+        uses highest-accuracy rule that applies, otherwise uses default
         """
         try:
             ds = self.decision_set
@@ -168,110 +175,120 @@ class DecisionSet():
                 preds.append(self.default_pred) # if no match, use default
         return np.array(preds)
 
-    def smooth_local_search(self, X, y, itemsets, d1=1/3, d2=1/3, d3=-1, max_est_iter=10):
+    def smooth_local_search(self, X, y, max_est_iter=10):
         """
         Performs smooth local search to optimize decision set
         """
-        domain = self._get_domain(itemsets, y)
-        print('Rules in domain = {}'.format(len(domain)))
-
-        print('Preprocessing...')
-        # cache the covers (just cache the length)
-        self.cover_cache = {}
-        for i, r in itemsets.iteritems():
-            self.cover_cache[r] = len(cover(X, r))
-
-        # cache the overlaps (just cache the length)
-        self.overlap_cache = {}
-        for i, ri in itemsets.iteritems():
-            for j, rj in itemsets.loc[i:].iteritems():
-                ov = len(overlap(X, ri, rj))
-                self.overlap_cache[(ri,rj)] = ov
-                self.overlap_cache[(rj,ri)] = ov # save both orders for ease of use
-
-        # cache correct cover indices as python set
-        self.correct_cover_cache = {}
-        for i, r in domain.iterrows():
-            xcov, ycov = correct_cover(X, y, r)
-            self.correct_cover_cache[(r['item'], r['class'])] = set(xcov.index)
-
-        # cache incorrect cover (just cache the length)
-        self.incorrect_cover_cache = {}
-        for i, r in domain.iterrows():
-            self.incorrect_cover_cache[(r['item'], r['class'])] = len(incorrect_cover(X, y, r)[0])
-
         # initialize an empty decision set
         A = pd.DataFrame([], columns=['item', 'class'])
         # sample a decision set with no bias
-        samp = self._sample_decision_set(domain, domain, 0) # line 4 of SLS algorithm
+        samp = self._sample_decision_set(self.domain, self.domain, 0) # line 4 of SLS algorithm
         # estimate the objective with the sample
-        opt = self._objective(X, y, itemsets, samp)
-        self.OPT = opt
-        self.error_bound = opt / (len(domain) ** 2)
-        print('Error bound = {}'.format(self.error_bound))
+        opt = self._objective(X, y, samp)
+        self.error_bound = opt / (len(self.domain) ** 2)
 
+        count = 0
         while True:
             print('Estimating w...')
-            w = self._estimate_w(domain, A, X, y, itemsets, d1=d1, maxiter=max_est_iter)
-            # print('w = {}'.format(w))
+            w = self._estimate_w(A, X, y, maxiter=max_est_iter)
 
-            print('Adding elements to A ')
-            A, add_done = self._add_elements(domain, A, w)
-            print('Current A:\n{}'.format(A))
+            A, add_done = self._add_elements(self.domain, A, w)
             if not add_done:
+                count += 1
                 continue # go back to top of loop
             else:
-                print('Removing elements from A')
-                A, remove_done = self._remove_elements(domain, A, w)
-                print('Current A:\n{}'.format(A))
+                A, remove_done = self._remove_elements(self.domain, A, w)
                 if not remove_done:
+                    count += 1
                     continue
                 else:
                     break
 
         # we run with d' = 1/3 and d' = -1 according to the paper (Section 4.2)
-        dset1 = self._sample_decision_set(domain, A, d2)
-        obj1 = self._objective(X, y, itemsets, dset1)
-        dset2 = self._sample_decision_set(domain, A, d3)
-        obj2 = self._objective(X, y, itemsets, dset2)
+        dset1 = self._sample_decision_set(self.domain, A, self.d2)
+        obj1 = self._objective(X, y, dset1)
+        dset2 = self._sample_decision_set(self.domain, A, self.d3)
+        obj2 = self._objective(X, y, dset2)
         return dset1, obj1, dset2, obj2
 
-    def mine_itemsets(self, X):
+    def cache(self, X, y):
         """
-        Performs frequent itemset mining using Apriori Algorithm
-        See here: http://rasbt.github.io/mlxtend/user_guide/frequent_patterns/apriori/
+        Caches various quantities to improve speed of main algorithm
+        """
+        print('Computing cache...')
 
-        Input:
-            X: pandas DataFrame, one-hot encoded
+        # cache itemsets & domain
+        self.itemsets = self.mine_itemsets(X)
+        self.domain = self._get_domain(self.itemsets, y)
+        print('Rules in domain: {}'.format(len(self.domain)))
 
-        Output:
-            itemsets: pandas DataFrame with support and itemsets columns
-        """
-        itemsets = apriori(X, min_support=self.min_support, use_colnames=True)
-        return itemsets.itemsets # apriori returns the support for each item. just take itemsets
+        # cache the covers (just cache the length)
+        self.cover_cache = {}
+        for i, r in self.itemsets.iteritems():
+            self.cover_cache[r] = len(cover(X, r))
+        print('Done caching covers')
 
-    def _get_domain(self, itemsets, y):
-        """
-        calculate cartesian product of itemset and classes, returns a dataframe
-        """
-        self.C = y.unique()
-        item_cross_class = itertools.product(itemsets, self.C)
-        domain = pd.DataFrame(item_cross_class, columns=['item', 'class'])
-        return domain
+        # cache the overlaps (just cache the length)
+        self.overlap_cache = {}
+        for i, ri in self.itemsets.iteritems():
+            for j, rj in self.itemsets.loc[i:].iteritems():
+                ov = len(overlap(X, ri, rj))
+                self.overlap_cache[(ri,rj)] = ov
+                self.overlap_cache[(rj,ri)] = ov # save both orders for ease of use
+        print('Done caching overlaps')
+
+        # cache correct cover indices as python set
+        self.correct_cover_cache = {}
+        for i, r in self.domain.iterrows():
+            xcov, ycov = correct_cover(X, y, r)
+            self.correct_cover_cache[(r['item'], r['class'])] = set(xcov.index)
+        print('Done caching correct cover')
+
+        # cache incorrect cover (just cache the length)
+        self.incorrect_cover_cache = {}
+        for i, r in self.domain.iterrows():
+            self.incorrect_cover_cache[(r['item'], r['class'])] = len(incorrect_cover(X, y, r)[0])
+        print('Done caching incorrect cover')
+
+        self.cache_exists = True
+
+    def get_cache(self):
+        """ helper to get a cache that has been fit """
+        if self.cache_exists:
+            return {
+                'itemsets': self.itemsets,
+                'domain': self.domain,
+                'cover': self.cover_cache,
+                'overlap': self.overlap_cache,
+                'correct_cover': self.correct_cover_cache,
+                'incorrect_cover': self.incorrect_cover_cache,
+            }
+        else:
+            raise RuntimeError("cache_exists False. Must set or compute cache first.")
+
+    def set_cache(self, cache_dict):
+        """ helper to set the cache according to a cache dict """
+        self.itemsets = cache_dict['itemsets']
+        self.domain = cache_dict['domain']
+        self.cover_cache = cache_dict['cover']
+        self.overlap_cache = cache_dict['overlap']
+        self.correct_cover_cache = cache_dict['correct_cover']
+        self.incorrect_cover_cache = cache_dict['incorrect_cover']
+        self.cache_exists = True
 
     def _add_elements(self, domain, A, w):
         """
         implements lines 9-12 of algorithm 1
         """
-        pre_shape = A.shape
+        pre_size = len(A)
         added = 0
         for i in range(len(domain)):
             r = domain.iloc[i]
             in_A = (A == r).all(axis=1).any()
-            if not in_A and w[i] > 2*self.error_bound:
+            if not in_A and w[i] > 2 * self.error_bound:
                 A = A.append(r)
                 added += 1
-        print('Executed _add_elements, shape change of A: {} -> {}'.format(pre_shape, A.shape))
+        print('Executed add elements, size of A: {} -> {}'.format(pre_size, len(A)))
         if added == 0:
             return A, True
         else:
@@ -281,17 +298,16 @@ class DecisionSet():
         """
         implements lines 13-15 of algorithm 1
         """
-        pre_shape = A.shape
+        pre_size = len(A)
         removed = 0
         for i in range(len(domain)):
             r = domain.iloc[i]
             in_A = (A == r).all(axis=1).any()
-            if in_A and w[i] < -2*self.error_bound:
+            if in_A and w[i] < -2 * self.error_bound:
                 to_drop = A.loc[(A == r).all(axis=1)]
-                print('dropping: {}'.format(to_drop))
                 A = A.drop(to_drop.index)
                 removed += 1
-        print('Executed _remove_elements, shape change of A: {} -> {}'.format(pre_shape, A.shape))
+        print('Executed remove elements, size of A: {} -> {}'.format(pre_size, len(A)))
         if removed == 0:
             return A, True
         else:
@@ -320,43 +336,63 @@ class DecisionSet():
         sample = pd.concat([in_samp, out_samp])
         return sample
 
-    def _objective(self, X, y, itemsets, decision_set):
+    def _estimate_w(self, A, X, y, maxiter=10):
+        """
+        performs the w estimation from line 5 of algorithm
+        """
+        w = []
+        for i, r in self.domain.iterrows():
+            std_error = float('inf')
+            diffs = []
+            j = 0
+            while std_error > self.error_bound and j < maxiter:
+                d_samp = self._sample_decision_set(self.domain, A, self.d1)
+                # remove r from sample
+                d_samp_drop = d_samp.append(r).drop_duplicates(keep=False)
+                # add it when we know it won't be duplicate
+                d_samp_with = d_samp_drop.append(r)
+
+                obj_with = self._objective(X, y, d_samp_with)
+                obj_drop = self._objective(X, y, d_samp_drop)
+
+                diffs.append(obj_with - obj_drop)
+                if len(diffs) >= 2:
+                    std_error = np.std(diffs)
+
+                j += 1
+            w.append(np.mean(diffs))
+        return w
+
+    def _objective(self, X, y, decision_set):
         """
         Computes the objective outlined in the Decision Set Paper
         """
         N = len(X)
         R = len(decision_set)
-        S = len(itemsets)
-
+        S = len(self.itemsets)
         # interpretability: less rules (possible for R > S ?)
         f1 = S - R
-
         # interpretability: short rules
         f2 = self.L_max * S - sum([len(r) for r in list(decision_set['item'])])
-
         # non-overlap: discourage overlap among rules
-        f3, f4 = self._non_overlap_objective(X, itemsets, decision_set)
-
+        f3, f4 = self._non_overlap_objective(X, decision_set)
         # coverage: at least one rule for each class
         f5 = self._coverage_objective(decision_set)
-
         # accuracy: reduce incorrect-cover (precision)
-        f6 = self._precision_objective(X, y, decision_set, itemsets)
-
+        f6 = self._precision_objective(X, y, decision_set)
         # accuracy: cover data points with at least one rule (recall)
         f7 = self._recall_objective(X, y, decision_set)
-
         objectives = [f1, f2, f3, f4, f5, f6, f7]
         # final objective: weighted sum of individuals
         return sum([o * l for o, l in zip(objectives, self.lambdas)])
 
-    def _non_overlap_objective(self, X, itemsets, decision_set):
+    def _non_overlap_objective(self, X, decision_set):
         """
         perform nested for-loop summation for calculating the non-overlap objective
         """
         N = len(X)
         R = len(decision_set)
-        S = len(itemsets)
+        S = len(self.itemsets)
 
         # calculate overlap for same and different classes separately
         same_class_overlap, diff_class_overlap = 0, 0
@@ -384,17 +420,17 @@ class DecisionSet():
         calculates coverage objective (f5 in paper)
         """
         obj = 0
-        for cls in self.C:
+        for cls in self.classes:
             have_rule = (decision_set['class']==cls).any(axis=0)
             obj += 1 if have_rule else 0
         return obj
 
-    def _precision_objective(self, X, y, decision_set, itemsets):
+    def _precision_objective(self, X, y, decision_set):
         """
         calculates precision objective (f6 in paper)
         """
         N = len(X)
-        S = len(itemsets)
+        S = len(self.itemsets)
         R = len(decision_set)
 
         items = list(decision_set['item'])
@@ -418,33 +454,6 @@ class DecisionSet():
 
         return len(idx)
 
-    def _estimate_w(self, domain, A, X, y, itemsets, d1=1/3, maxiter=10):
-        """
-        performs the w estimation from line 5 of algorithm
-        """
-        w = []
-        for i, r in domain.iterrows():
-            std_error = float('inf')
-            diffs = []
-            j = 0
-            while std_error > self.error_bound and j < maxiter:
-
-                d_samp1 = self._sample_decision_set(domain, A, d1)
-                d_samp_with = d_samp1.append(r).drop_duplicates(keep='first')
-                d_samp_drop = d_samp_with.append(r).drop_duplicates(keep=False)
-
-                obj_with = self._objective(X, y, itemsets, d_samp_with)
-                obj_drop = self._objective(X, y, itemsets, d_samp_drop)
-
-                diffs.append(obj_with - obj_drop)
-
-                if len(diffs) >= 2:
-                    std_error = np.std(diffs)
-                j += 1
-
-            w.append(np.mean(diffs))
-        return w
-
     def _remove_duplicate_itemsets(self):
         """
         removes any duplicate itemsets (i.e. itemsets which predict different classes)
@@ -453,3 +462,24 @@ class DecisionSet():
         """
         self.decision_set = self.decision_set.loc[self.decision_set['item'].drop_duplicates(keep='first').index]
 
+    def mine_itemsets(self, X):
+        """
+        Performs frequent itemset mining using Apriori Algorithm
+        See here: http://rasbt.github.io/mlxtend/user_guide/frequent_patterns/apriori/
+
+        Input:
+            X: pandas DataFrame, one-hot encoded
+
+        Output:
+            itemsets: pandas DataFrame with support and itemsets columns
+        """
+        itemsets = apriori(X, min_support=self.min_support, use_colnames=True)
+        return itemsets.itemsets # apriori returns the support for each item. just take itemsets
+
+    def _get_domain(self, itemsets, y):
+        """
+        calculate cartesian product of itemset and classes, returns a dataframe
+        """
+        item_cross_class = itertools.product(itemsets, y.unique())
+        domain = pd.DataFrame(item_cross_class, columns=['item', 'class'])
+        return domain
